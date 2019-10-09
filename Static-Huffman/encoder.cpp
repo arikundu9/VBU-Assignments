573dcfa4427d28ee5e282132a93ef337de385a9c
ARIJIT KUNDU <arikundu9@gmail.com>
2019-10-09 11:59:27 +0530

    encoder updated [10] [30]

--- Static-Huffman/encoder.cpp
+++ Static-Huffman/encoder.cpp
@@ -1,36 +1,32 @@
 #include<iostream>
 #include<fstream>
+#define MX_SMBL_L 10
+#define MX_CODW_L 30
 using namespace std;
 
-typedef struct {
-    char data:1;
-} Bit;
-
 typedef struct{
-    char *operators;
-    char (*codewords)[30];
+    char (*symbols)[MX_SMBL_L];
+    char (*codewords)[MX_CODW_L];
+    int nOr;
 } container;
 
-bool readCodeFile(container *C/* char **operators, char (**codewords)[30] */ /* char ***codewords */,int *N) {
+bool readCodeFile(container *TAB) {
     ifstream codetab("H_codes.txt");
     string line;
-    int i=0,nOr;
+    int i=0;
     if(codetab.fail()) {
         cout<<"\tERROR :: Unable To Read Codeword File!! [H_codes.txt]\n";
         return false;
     } else {
         i=0;
-        codetab>>nOr;
-        *N=nOr;
-        C->operators=new char[nOr];
-        C->codewords=new char[nOr][30];
+        codetab>>TAB->nOr;
+        TAB->symbols=new char[TAB->nOr][MX_SMBL_L];
+        TAB->codewords=new char[TAB->nOr][MX_CODW_L];
         while(codetab) {
             getline(codetab,line);
-            if(sscanf(line.c_str(),"%c %s",(C->operators)+i,(C->codewords)[i]) > 0) {
+            if(sscanf(line.c_str(),"%s %s",(TAB->symbols)[i],(TAB->codewords)[i]) > 0) {
                 i++;
             }
-            /* codetab>>tmp_kw>>freq;
-            cout<<tmp_kw<<"="<<freq<<endl; */
         }
         codetab.close();
     }
@@ -39,39 +35,43 @@ bool readCodeFile(container *C/* char **operators, char (**codewords)[30] */ /* 
 
 void encode(const char *iFile,
             const char *oFile,
-            char *operators,
-            char (*codewords)[30],
+            char (*symbols)[MX_SMBL_L],
+            char (*codewords)[MX_CODW_L],
             int nOr){
     ifstream source(iFile);
     ofstream outf(oFile,ios::binary);
     char chr;
-    int i;
-    char *p;
-    Bit B;
+    int i,j=0;
+    bool indicator[nOr],flag=false;
+
     if(source.fail()) {
         cout<<"\tERROR :: Unable To Read Source File!! ["<<iFile<<"]\n";
         return;
     }
     while(source and source>>chr){
         for(i=0;i<nOr;i++){
-            if(operators[i]==chr){
-               /*  p=codewords[i];
-                while(*p!='\0'){
-                    if(*p=='1'){
-                        B.data=0b1;
-                        //outf<<'a';
-                        outf.write((char*)&B,sizeof(B));
-                    }
-                    else if(*p=='0'){
-                        B.data=0b0;
-                        //outf<<'b';
-                        outf.write((char*)&B,sizeof(B));
-                    }
-                    p++;
-                } */
+            if(j==0){
+                if(symbols[i][0]==chr)
+                    indicator[i]=true;
+                else
+                    indicator[i]=false;
+            }
+            else{
+                if(symbols[i][j]!=chr and indicator[i])
+                    indicator[i]=false;
+            }
+            if(symbols[i][j+1]=='\0' and indicator[i]){
                 outf<<codewords[i];
+                flag=true;
                 break;
             }
+        }
+        if(flag){
+            j=0;
+            flag=false;
+        }
+        else{
+            j++;
         }
     }
 }
@@ -81,17 +81,12 @@ int main(int argc,char *argv[]){
         cout<<"\t:::: Huffman Encoder ::::\nUsage: ./a.out [INPUT-FILE-NAME] [OUTPUT-FILE-NAME]\n";
         exit(0);
     }
-    char *operators;
-    char (*codewords)[30];
-    int i,nOr;
-    container C;
-    if(readCodeFile(&C,&nOr)){
-        operators=C.operators;
-        codewords=C.codewords;
-        /* for(i=0;i<nOr;i++){
-            cout<<operators[i]<<" - "<<codewords[i]<<endl;
+    container codeTable;
+    if(readCodeFile(&codeTable)){
+        /* for(int i=0;i<codeTable.nOr;i++){
+            cout<<codeTable.symbols[i]<<" = "<<codeTable.codewords[i]<<endl;
         } */
-        encode(argv[1],argv[2],operators,codewords,nOr);
+        encode(argv[1], argv[2], codeTable.symbols, codeTable.codewords, codeTable.nOr);
     }
     return 0;
 }
