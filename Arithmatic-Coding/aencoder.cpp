1ea1855992004cbce8154f6bd94707c7c0c03f6d
ARIJIT KUNDU <arikundu9@gmail.com>
2019-10-23 21:24:04 +0530

    Arithmatic-Coding Added

---
+++ Arithmatic-Coding/aencoder.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(int argc,char *argv[]){
+    if(argc!=2){
+        cout<<"\t:::: Arithmatic Encoder ::::\nUsage: ./aencoder [INPUT-FILE-NAME] [OUTPUT-FILE-NAME]\n";
+        exit(0);
+    }
+}
